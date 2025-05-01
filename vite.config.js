a3cc09bc4060861cfbfba16c5243d449b436a2be
han-link <115926359+han-link@users.noreply.github.com>
2025-05-01 14:25:48 +0200

    update project structure and vite config

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+export default {
+    root: "./src",
+    build: {
+        outDir: "../dist",
+        emptyOutDir: true,
+        rollupOptions: {
+            input: {
+                index: "./src/index.html",
+                about: "./src/tictactoe.html",
+            },
+        },
+    },
+};
