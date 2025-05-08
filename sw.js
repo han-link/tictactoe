6695857fe630266a205f156b9e9380bbefb8c3d2
han-link <115926359+han-link@users.noreply.github.com>
2025-05-08 23:46:36 +0000

    Build: (bc539d4) Merge pull request #1 from han-link/dev
     Add PWA, Navbar, and New Game Rule

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = `temperature-converter-v1`;
+
+self.addEventListener('install', event => {
+    event.waitUntil((async () => {
+        const cache = await caches.open(CACHE_NAME);
+        await cache.addAll([
+            '/',
+            '/assets/index.js',
+            '/assets/index.css',
+            '/burger.svg',
+            '/donut.svg',
+            '/github.svg',
+            '/tic-tac-toe-game.svg'
+        ]);
+    })());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith((async () => {
+        const cache = await caches.open(CACHE_NAME);
+
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse) {
+            return cachedResponse;
+        } else {
+            try {
+                const fetchResponse = await fetch(event.request);
+
+                await cache.put(event.request, fetchResponse.clone());
+                return fetchResponse;
+            } catch (e) {}
+        }
+    })());
+});
